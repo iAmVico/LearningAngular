fe45cbc15733a5f5ef4def9a1cfbc12af34de0f9
Victor Tihomirov <Victor.Tihomirov@realdolmen.com>
2020-03-22 18:26:00 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './features/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './features/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './features/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './features/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './features/recipes/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './features/recipes/recipes.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
