6edaa6d80bd09644e20673ee8663078a1c25ad09
Victor <victor.tihomirov@student.vives.be>
2020-04-01 11:15:26 +0200

    section 3

--- src/app/features/shopping-list/shopping-list.component.ts
+++ src/app/features/shopping-list/shopping-list.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Ingridient } from "src/app/shared/ingridient.model";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: "app-shopping-list",
+    templateUrl: "./shopping-list.component.html",
+    styleUrls: ["./shopping-list.component.css"]
 })
 export class ShoppingListComponent implements OnInit {
+    ingridients: Ingridient[] = [
+        new Ingridient("Apples", 5),
+        new Ingridient("Tomatoes", 10)
+    ];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
