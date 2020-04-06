5bdc186b51a72376b8a975295b0d3e9a8a0e6fa4
Victor <victor.tihomirov@student.vives.be>
2020-04-06 10:44:27 +0200

    Completed Section 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './features/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './features/shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './features/recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './features/recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './features/recipes/recipe-detail/recipe-detail.component';
-import { RecipeComponent } from './features/recipes/recipes.component';
-import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
 
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+
 @NgModule({
-    declarations: [
-        AppComponent,
-        ShoppingListComponent,
-        ShoppingListEditComponent,
-        RecipeListComponent,
-        RecipeItemComponent,
-        RecipeDetailComponent,
-        RecipeComponent,
-        HeaderComponent
-    ],
-    imports: [BrowserModule, AppRoutingModule, FormsModule],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    DropdownDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [ShoppingListService],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
